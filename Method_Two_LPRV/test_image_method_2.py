d5158dddf38e25eedb2afe7b6fb2b3764f2283bd
leenock <leenock176@gmail.com>
2022-08-13 00:12:56 +0800

    NoneType Error

--- Method_Two_LPRV/test_image_method_2.py
+++ Method_Two_LPRV/test_image_method_2.py
@@ -6,9 +6,9 @@ import cv2
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract'
 
 # Load image, create blank mask, convert to HSV, define thresholds, color threshold
-test_image = cv2.imread('YvuWB.png')
-result = np.zeros(test_image.shape, dtype=np.uint8)
-hsv = cv2.cvtColor(test_image, cv2.COLOR_BGR2HSV)
+image = cv2.imread('YvuWB.png')
+result = np.zeros(image.shape, dtype=np.uint8)
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 lower = np.array([0,0,0])
 upper = np.array([179,100,130])
 mask = cv2.inRange(hsv, lower, upper)
@@ -19,3 +19,24 @@ kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
 close = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
 extract = cv2.merge([close,close,close])
 cv2.imshow('extract', extract)
+
+# Find contours, filter using contour area, and extract using Numpy slicing
+cnts = cv2.findContours(close, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    x,y,w,h = cv2.boundingRect(c)
+    area = w * h
+    if area < 5000 and area > 2500:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
+        result[y:y+h, x:x+w] = extract[y:y+h, x:x+w]
+
+        # Invert image and throw into Pytesseract
+invert = 255 - result
+data = pytesseract.image_to_string(invert, lang='eng',config='--psm 6')
+print(data)
+
+cv2.imshow('image', image)
+cv2.imshow('close', close)
+cv2.imshow('result', result)
+cv2.imshow('invert', invert)
+cv2.waitKey()
